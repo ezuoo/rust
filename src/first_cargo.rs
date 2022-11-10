9731f1df1e69ae32b12b0a6a03a209aad0b63fd8
ezuoo <juho0003@naver.com>
2022-11-10 10:28:50 +0900

    First init

---
+++ src/first_cargo.rs
@@ -0,0 +1,5 @@
+pub fn first_cargo() {
+  println!("Another fn");
+  println!("Hello World !! ");
+  println!("# First cargo # ");
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+mod first_cargo;
+fn main() {
+    println!("Hello, world!");
+    first_cargo::first_cargo();
+}
