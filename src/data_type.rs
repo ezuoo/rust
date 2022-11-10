e12f43202810916af87de91348507db95b8fdcc4
ezuoo <juho0003@naver.com>
2022-11-10 10:43:35 +0900

    Data type

---
+++ src/data_type.rs
@@ -0,0 +1,5 @@
+pub fn data_type() {
+  let x: [i32; 3] = [1, 2, 3];
+
+  println!("{:?}", x);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 // mod first_cargo;
-mod guess_number;
+// mod guess_number;
+mod data_type;
 fn main() {
     println!("Hello, world!");
     // first_cargo::first_cargo();
-    guess_number::guess_number();
+    // guess_number::guess_number();
+    data_type::data_type();
 }
