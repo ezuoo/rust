48ba157862a07916a9faa747e76545eefca78252
ezuoo <juho0003@naver.com>
2022-11-10 10:36:25 +0900

    Guess number

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-mod first_cargo;
+// mod first_cargo;
+mod guess_number;
 fn main() {
     println!("Hello, world!");
-    first_cargo::first_cargo();
+    // first_cargo::first_cargo();
+    guess_number::guess_number();
 }
