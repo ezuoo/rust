6ad47fc07614d821646d0382cb8f6e20a583f8bc
ezuoo <juho0003@naver.com>
2022-11-10 12:22:08 +0900

    variables

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 // mod first_cargo;
 // mod guess_number;
-mod data_type;
+// mod data_type;
+mod variables;
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+
     // first_cargo::first_cargo();
     // guess_number::guess_number();
-    data_type::data_type();
+    // data_type::data_type();
+
+    variables::variables();
 }
---
+++ src/variables.rs
@@ -0,0 +1,34 @@
+pub fn variables() {
+  // let x = 5;
+  // error[E0384]: cannot assign twice to immutable variable `x`
+
+  let mut x = 5;
+  println!("The value of x is: {x}");
+  x = 6;
+  println!("The value of x is: {x}");
+  // #################################################################
+
+  const MAX_POINTS: u32 = 100_000;
+
+  println!("Max points : {MAX_POINTS}");
+
+  // #################################################################
+
+  let shadowed_values = 5;
+  let shadowed_values = shadowed_values + 1;
+  let shadowed_values = shadowed_values * 2;
+
+  println!("Shadowed_values is: {}", shadowed_values);
+
+  // #################################################################
+
+  // let mut spaces = "   ";
+  // spaces = spaces.len();
+  // error[E0308]: mismatched types
+
+  let spaces = "   ";
+  let spaces = spaces.len();
+  println!("Spaces is: {}", spaces);
+
+  // #################################################################
+}
