d2638644810396bda883b7f3a73cc9eb19153671
ezuoo <juho0003@naver.com>
2022-11-10 12:56:40 +0900

    Data Types

--- src/data_type.rs
+++ src/data_type.rs
@@ -1,5 +1,14 @@
 pub fn data_type() {
-  let x: [i32; 3] = [1, 2, 3];
+  // let guess = "42".parse().expect("Not a number!");
+  // error[E0282]: type annotations needed
 
-  println!("{:?}", x);
+  let guess: u32 = "42".parse().expect("Not a number!");
+  println!("The value of variable : {guess}");
+
+  let tuple: (i32, f64, u8) = (123, 1.1, 5);
+  println!("The value of tuple : {:?}", tuple);
+
+  // let x: [i32; 3] = [1, 2, 3];
+  let array = [1, 2, 3];
+  println!("The value of array : {:?}", array);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 // mod first_cargo;
 // mod guess_number;
-// mod data_type;
-mod variables;
+// mod variables;
+mod data_type;
 fn main() {
     // println!("Hello, world!");
 
     // first_cargo::first_cargo();
     // guess_number::guess_number();
-    // data_type::data_type();
-
-    variables::variables();
+    // variables::variables();
+    data_type::data_type();
 }
