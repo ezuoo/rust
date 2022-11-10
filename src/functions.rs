1411ea1a99756ea76ed80f5310adf0739c1659aa
ezuoo <juho0003@naver.com>
2022-11-10 16:04:30 +0900

    functions

---
+++ src/functions.rs
@@ -0,0 +1,7 @@
+pub fn functions(argu: u32) -> u32 {
+  println!("The value of arguments : {argu}");
+
+  argu * 2
+  // argu * 2;
+  // error[E0308]: mismatched types
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 // mod first_cargo;
 // mod guess_number;
 // mod variables;
-mod data_type;
+// mod data_type;
+mod functions;
 fn main() {
     // println!("Hello, world!");
 
     // first_cargo::first_cargo();
     // guess_number::guess_number();
     // variables::variables();
-    data_type::data_type();
+    // data_type::data_type();
+    let return_value = functions::functions(32);
+    println!("The value of function's return : {return_value}");
 }
