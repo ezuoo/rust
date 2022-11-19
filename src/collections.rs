c100939248f3ee3f37c2030557e41134a833e558
ezuoo <juho0003@naver.com>
2022-11-19 15:01:23 +0900

    collections - string

--- src/collections.rs
+++ src/collections.rs
@@ -1,9 +1,11 @@
 pub fn collections_main() {
   println!("## Collections ##");
 
-  vector();
+  // vector();
+  // string();
 }
 
+/*
 fn vector() {
   println!("# Vector #");
   let vector: Vec<i32> = Vec::new();
@@ -37,3 +39,42 @@ fn vector() {
   v1.pop();
   println!("v1 : {:?}", v1);
 }
+ */
+
+/*
+fn string() {
+  println!("# String #");
+
+  //////////// create empty string ////////////
+  // let mut string = String::new();
+
+  //////////// string literal to string ////////////
+  // let string_literal = "initial contents";
+  // let string_from_string_literal = String::from(string_literal);
+  // let string_literal_to_string = string_literal.to_string();
+  // let string_literal_to_string = "initial contents".to_string();
+
+  //////////// renew string ////////////
+  // let mut string = String::from("string");
+  // let new_string = "new string";
+
+  // string.push_str(&new_string);
+
+  // println!("string : {}", string);
+  // println!("new_string : {}", new_string);
+
+  // let s1 = String::from("hello, ");
+  // let s2 = String::from("world !");
+  // let s3 = s1 + &s2;
+  // println!("s3 : {s3}")
+
+  //////////// string index ////////////
+  // let s1 = String::from("hello");
+
+  // error[E0277]: the type `String` cannot be indexed by `{integer}`
+  // let h = s1[0];
+
+  // println!("s1 length : {}", s1.len());
+  // println!("이주호 length : {}", "이".len());
+}
+*/
